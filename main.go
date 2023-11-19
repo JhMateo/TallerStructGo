2901f877c024e892f22a5e302904ed76b05ee4ed
JhMateo <jmgranada@unillanos.edu.co>
2023-11-19 15:53:34 -0500

    Puntos completos

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	// 1. Estudiante con mejor promedio:
 	fmt.Println("---- 1. MEJOR PROMEDIO ----")
 	taller.MejorPromedio(estudiantes)
@@ -31,6 +30,17 @@ func main() {
 	// 4. Top 10 estudiantes con peores notas de cada curso
 	fmt.Println("\n---- 4. TOP 10 ESTUDIANTES CON PEORES NOTAS DE CADA CURSO ----")
 	taller.Top10PeoresPorCurso(estudiantes)
+
+	// 5. Estudiante Masculino con mayor edad
+	fmt.Println("\n---- 5. ESTUDIANTE MASCULINO DE MAYOR EDAD ----")
+	taller.ImprimirEstudianteMasculino(estudiantes)
+
+	// 6. Estudiante Femenino con mayor edad
+	fmt.Println("\n---- 6. ESTUDIANTE FEMENINO DE MAYOR EDAD ----")
+	taller.ImprimirEstudianteFemenino(estudiantes)
+
+	fmt.Println("\n---- 7. PROMEDIO, RANGO, VARIANZA Y DESVIACIÓN ESTANDAR PARA CADA CURSO ----")
+	taller.ImprimirEstadisticasPorCurso(estudiantes)
 
 	// 8. Reporte de estudiantes que se matricularon en el año pasado (2022).
 	fmt.Println("\n---- 8. REPORTE DE ESTUDIANTES QUE SE MATRICULARON EN EL AÑO PASADO (2022) ----")
